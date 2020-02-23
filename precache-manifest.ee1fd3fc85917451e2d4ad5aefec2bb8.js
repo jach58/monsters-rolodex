f1fd2bfa92ae80e642aadb5cb8225bd1a7511ee8
Juan Castillo <jach58@hotmail.com>
2020-02-22 20:12:18 -0600

    Updates

---
+++ precache-manifest.ee1fd3fc85917451e2d4ad5aefec2bb8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56c4523d08a0b52edb9ae687abbc287b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "72b54936d768075b047d",
+    "url": "/monsters-rolodex/static/css/main.c4f9fd72.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b54936d768075b047d",
+    "url": "/monsters-rolodex/static/js/main.8c6c2ad2.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
